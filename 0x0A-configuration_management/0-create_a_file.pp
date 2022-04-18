a2a025406757365c9ab370c2468fd0a1eedaf739
Tukson <santiagonegreyra@gmail.com>
2022-04-18 13:20:41 -0700

    fixing create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,12 +1,9 @@
 #create file puppet
 
-node default {
-    file { 'school':
-        ensure  => 'present',
-        path    => '/tmp/school',
-        mode    => '0744',
-        owner   => 'www-data',
-        group   => 'www-data',
-        content => 'I love Puppet'
-    }
+file { 'create file':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
