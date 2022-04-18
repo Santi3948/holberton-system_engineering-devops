95c3b63bb2c8e36a5746a41bcaeef3d00151522d
Tukson <santiagonegreyra@gmail.com>
2022-04-18 13:16:17 -0700

    0 create file works locally

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,12 @@
+#create file puppet
+
+node default {
+    file { 'school':
+        ensure  => 'present',
+        path    => '/tmp/school',
+        mode    => '0744',
+        owner   => 'www-data',
+        group   => 'www-data',
+        content => 'I love Puppet'
+    }
+}
